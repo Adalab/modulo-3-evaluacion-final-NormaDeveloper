b4a568960f6cd9f7aecda0a8ad4e2cf96604168c
Norma <normarivasmadrid@gmail.com>
2022-01-28 20:54:29 +0100

    add background picture to each character card

--- src/components/Character.js
+++ src/components/Character.js
@@ -6,13 +6,15 @@ import propTypes from 'prop-types';
 const Character = (props) => {
   return (
     <Link to={`/character/${props.character.id}`} className="character__link">
-      <img
-        src={props.character.photo}
-        alt="character"
-        className="characterImg"
-      />
-      <h3>{props.character.name}</h3>
-      <p>{speciesTranslate(props.character.species)}</p>
+      <div className="cardConatainer">
+        <img
+          src={props.character.photo}
+          alt="character"
+          className="characterImg"
+        />
+        <h3>{props.character.name}</h3>
+        <p>{speciesTranslate(props.character.species)}</p>
+      </div>
     </Link>
   );
 };
